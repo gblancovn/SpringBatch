fa5bd0d01c3d6671ac29bae8b81b71af592a1ead
Marco Carreiro González <marcocarreiro14@gmail.com>
2024-05-08 10:30:43 +0200

    Listeners en job-context y listeners en main

---
+++ src/main/java/es/viewnext/listener/MyJobExecutionListener.java
@@ -0,0 +1,21 @@
+package es.viewnext.listener;
+
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+
+public class MyJobExecutionListener implements JobExecutionListener{
+	@Override
+    public void beforeJob(JobExecution jobExecution) {
+        System.out.println("Before job execution....");
+    }
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+ 
+    	System.out.println("After job execution...");
+    	System.out.println("job Status: " + jobExecution.getStatus());
+    	System.out.println("Job exitstatus: " + jobExecution.getExitStatus());
+        jobExecution.setExitStatus(ExitStatus.COMPLETED);
+    }
+}
---
+++ src/main/java/es/viewnext/listener/MyStepExecutionListener.java
@@ -0,0 +1,18 @@
+package es.viewnext.listener;
+
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
+
+public class MyStepExecutionListener implements StepExecutionListener{
+	@Override
+    public void beforeStep(StepExecution stepExecution) {
+        System.out.println("Befores step execution... ");
+    }
+
+    @Override
+    public ExitStatus afterStep(StepExecution stepExecution) {
+    	System.out.println("after step execution... ");
+		return null;
+    }
+}
